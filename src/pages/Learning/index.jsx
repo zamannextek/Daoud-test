a23610fefed59721ab6acd7f97d832cd5904f450
zamannextek <nextekzaman@gmail.com>
2025-06-26 22:44:34 +0500

    leARN useState

--- src/pages/Learning/index.jsx
+++ src/pages/Learning/index.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const Learning = () => {
+  const [count, setCount] = useState(0);
+
+  function CountUpdate() {
+    const ncount = count + 1;
+    setCount(ncount);
+  }
+  const CountDec = () => {
+    setCount(count - 1);
+  };
+  const CountAdd2 = () => {
+    setCount((prv) => prv + 2);
+  };
   return (
     <div>
-      <h1>Learning</h1>
+      <button onClick={() => setCount((prv) => prv - 2)}>-2</button>
+      <button onClick={() => CountDec()}>-</button>
+      {count}
+      <button onClick={() => CountUpdate()}>+</button>
+      <button onClick={() => CountAdd2()}>+2</button>
+       <button onClick={()=>setCount((prv) => prv + 3)}>+3</button>
     </div>
   );
 };
