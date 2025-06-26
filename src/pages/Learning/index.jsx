a207d94a433638b153f0782947f77f4d7468e112
zamannextek <nextekzaman@gmail.com>
2025-06-26 22:55:18 +0500

    toggel also done

--- src/pages/Learning/index.jsx
+++ src/pages/Learning/index.jsx
@@ -1,26 +1,20 @@
 import { useState } from "react";
 
 const Learning = () => {
-  const [count, setCount] = useState(0);
+  const [status, setStatus] = useState("login");
 
-  function CountUpdate() {
-    const ncount = count + 1;
-    setCount(ncount);
-  }
-  const CountDec = () => {
-    setCount(count - 1);
+  const toggelStatus = () => {
+    if (status == "login") {
+      setStatus("signup");
+    } else {
+      setStatus("login");
+    }
   };
-  const CountAdd2 = () => {
-    setCount((prv) => prv + 2);
-  };
+
   return (
     <div>
-      <button onClick={() => setCount((prv) => prv - 2)}>-2</button>
-      <button onClick={() => CountDec()}>-</button>
-      {count}
-      <button onClick={() => CountUpdate()}>+</button>
-      <button onClick={() => CountAdd2()}>+2</button>
-       <button onClick={()=>setCount((prv) => prv + 3)}>+3</button>
+      <button onClick={() => toggelStatus()}>toggel</button>
+      {status == "signup" ? <div>signup</div> : <div>login</div>}
     </div>
   );
 };
