a9f6fb4dfe29c4cd25cec59a166dd0b8da50d74b
MuhammadDaoud38 <muhammaddaoud207@gmail.com>
2025-06-27 09:50:55 +0500

    changs

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import "./app.css";
 import AlramSplash from "./pages/Alarm";
 import AlarmDashboard from "./pages/AlramDashboard";
 import Calculate from "./pages/Calculater";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import Setalarm from "./pages/setalarm";
 import Setbedtime from "./pages/setbedtime";
-
+import Registrationpage from "./pages/Registrationpage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path= "/registrationpage" element= {< Registrationpage />} />
         <Route path="/" element={<Calculate />} />
         <Route path="/splash" element={<AlramSplash />} />
         <Route path="/alram-dashboard" element={<AlarmDashboard />} />
---
+++ src/pages/Registrationpage/index.js
@@ -0,0 +1,6 @@
+import "./index.css";
+const Registrationpage = () => {
+  return <>hh</>;
+};
+
+export default Registrationpage;
